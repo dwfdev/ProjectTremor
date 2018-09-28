12e3787d46d10f3753e955bc0f61d3141b1767ea
DenverLacey <36467790+DenverLacey@users.noreply.github.com>
2018-09-28 23:43:37 +1000

    Added EnemyBullet collision to PlayerActor and eLifeState enum and life state functionality. - OnTriggerEnter function to handle bullet collision. - Respawn function added to handle respawning. - m_fDyingTimer and m_fDyingTimerSeconds variables added for dying timer. - m_fRespawnInvincibilityTimer and m_fRespawnInvincibilityTimerSeconds variables added for respawn timer. - Changes made by Denver.

--- PTSwiftSorcery/Assets/Scripts/Player/PlayerActor.cs
+++ PTSwiftSorcery/Assets/Scripts/Player/PlayerActor.cs
@@ -1,34 +1,92 @@
 ﻿///<summary>
 ///		Script Manager:	Denver
 ///		Description:	Handles the movement of the player using the mouse
-///		Date Modified:	20/09/2018
+///		Date Modified:	28/09/2018
 ///</summary>
 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum eLifeState {
+	DEAD,
+	DYING,
+	NORMAL,
+	SHIELDED,
+	INVINCIBLE
+}
+
 public class PlayerActor : MonoBehaviour
 {
+	[Tooltip("Amount of lives the player has.")]
+	public int m_nLives;
+
+	[Tooltip("Time, in seconds player has to recover from being hit.")]
+	[SerializeField] private float m_fDyingTimerSeconds;
+
+	[Tooltip("Time, in seconds player has invinciblity after respawn.")]
+	[SerializeField] private float m_fRespawnInvincibilityTimerSeconds;
+
+	[HideInInspector]
+	public eLifeState m_lifeState;
+
 	[Tooltip("Scales raw mouse movement.")]
-	[SerializeField]
-	private float fMouseSensitivity;
+	[SerializeField] private float fMouseSensitivity;
 
 	[Tooltip("Lessens jitter. Too high a value makes it unresponsive.")]
-	[SerializeField]
-	private float fMouseSmoothing;
+	[SerializeField] private float fMouseSmoothing;
 
 	private Vector3 v3MouseSmooth;
 
+	private float m_fDyingTimer;
+	private bool m_bDyingTimerIsActive;
+
+	private float m_fRespawnInvincibilityTimer;
+	private bool m_bRespawnInvincibilityTimerIsActive;
+
 	// Use this for initialization
-	void Start()
-	{
+	void Start() {
+
 		gameObject.tag = "Player";
+
+		m_lifeState = eLifeState.NORMAL;
+
 	}
 
 	// Update is called once per frame
-	void Update()
-	{
+	void Update() {
+
+		#region Timers
+		// if player is dying
+		if (m_bDyingTimerIsActive) {
+			// count down dying timer
+			m_fDyingTimer -= Time.deltaTime;
+		
+			// if timer is up
+			if (m_fDyingTimer <= 0f) {
+				// deactivate timer
+				m_bDyingTimerIsActive = false;
+
+				Respawn();
+			}
+		}
+
+		// if player has just respawned
+		if (m_bRespawnInvincibilityTimerIsActive) {
+			// count down invincibility timer
+			m_fRespawnInvincibilityTimer -= Time.deltaTime;
+		
+			// if timer is up
+			if (m_fRespawnInvincibilityTimer <= 0f) {
+				// return player to normal
+				m_lifeState = eLifeState.NORMAL;
+
+				// deactivate timer
+				m_bRespawnInvincibilityTimerIsActive = false;
+			}
+		}
+
+		#endregion
 
 		#region Mouse Movement
 		// get the movement of the mouse
@@ -44,6 +102,58 @@ public class PlayerActor : MonoBehaviour
 		// move player
 		transform.Translate(v3MouseSmooth * Time.deltaTime);
 		#endregion
+
+		// if the player is dead
+		if (m_lifeState == eLifeState.DEAD) {
+			// destroy the player
+			Destroy(gameObject);
+		}
+	}
+
+	void OnTriggerEnter(Collider info) {
+
+		// check that collision was with a bullet
+		if (info.gameObject.tag == "EnemyBullet") {
+
+			Debug.Log(name + " has collided with an EnemyBullet");
+
+			// if player is shielded
+			if (m_lifeState == eLifeState.SHIELDED) {
+				// set life state to normal
+				m_lifeState = eLifeState.NORMAL;
+			}
+			else if (m_lifeState == eLifeState.NORMAL) {
+				// set life state to dying
+				m_lifeState = eLifeState.DYING;
+
+				// start dying timer
+				m_bDyingTimerIsActive = true;
+				m_fDyingTimer = m_fDyingTimerSeconds;
+			}
+		}
+		
+	}
+
+	void Respawn() {
+		
+		// decrement lives
+		--m_nLives;
+
+		// if player has no more lives
+		if (m_nLives == 0) {
+			// player is dead
+			m_lifeState = eLifeState.DEAD;
+		}
+		else {
+			// reset poisition
+
+			// give player invinciblity
+			m_lifeState = eLifeState.INVINCIBLE;
+			
+			// start respawn timer
+			m_bRespawnInvincibilityTimerIsActive = true;
+			m_fRespawnInvincibilityTimer = m_fRespawnInvincibilityTimerSeconds;
+		}
 
 	}
 }
