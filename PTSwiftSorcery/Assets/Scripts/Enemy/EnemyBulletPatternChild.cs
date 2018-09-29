96d89da5cb099959dad6289efe3671a9a272ca1c
ThePyromage <the.pyromagi@gmail.com>
2018-09-29 13:47:25 +1000

    Added basic framework for pattern child and manager -Framework for child and manager relationship added

--- PTSwiftSorcery/Assets/Scripts/Enemy/EnemyBulletPatternChild.cs
+++ PTSwiftSorcery/Assets/Scripts/Enemy/EnemyBulletPatternChild.cs
@@ -26,21 +26,54 @@ public class EnemyBulletPatternChild : MonoBehaviour
 	[Tooltip("How long since the pattern fires until this object should fire it's projectile in seconds")]
 	[SerializeField] private float m_fTimer;
 
+	//The current time in seconds
+	private float m_fCurrentTimer;
+
 	[Tooltip("How fast the projectile this fires moves")]
 	[SerializeField] private float m_fMoveSpeed;
 
 	[Tooltip("What type of shot this pattern child shoots")]
 	[SerializeField] private eBulletType m_eBulletType;
 
+	//Whether or not this pattern child is currently active
+	private bool m_bActive;
+
 	// Use this for initialization
-	void Start ()
+	void Awake()
 	{
-		
+		m_fCurrentTimer = 0.0f;
 	}
 	
 	// Update is called once per frame
-	void Update ()
+	void Update()
 	{
-		
+		if(m_bActive)
+		{
+			m_fCurrentTimer += Time.deltaTime;
+
+			if(m_fCurrentTimer >= m_fTimer)
+			{
+				Fire();
+				m_fCurrentTimer = 0.0f;
+				m_bActive = false;
+			}
+		}
+	}
+
+	//Called at the start of the pattern
+	public void StartPattern()
+	{
+		m_bActive = true;
+	}
+
+
+	public float GetTimer()
+	{
+		return m_fTimer;
+	}
+
+	private void Fire()
+	{
+		//code for firing shot
 	}
 }
--- PTSwiftSorcery/Assets/Scripts/Enemy/EnemyBulletPatternManager.cs
+++ PTSwiftSorcery/Assets/Scripts/Enemy/EnemyBulletPatternManager.cs
@@ -9,21 +9,48 @@ using UnityEngine;
 ///</summary>
 public class EnemyBulletPatternManager : MonoBehaviour
 {
-	[Tooltip("How long between pattern firing in seconds. ")]
+	[Tooltip("How long between pattern firing in seconds. \nThis should always be higher or equal to the longest child timer, or else bad stuff can happen")]
 	[SerializeField] private float m_fTimer;
+
+	//The current time in seconds
+	private float m_fCurrentTimer;
 
 	[Tooltip("All spawn locations of bullets in this pattern")]
 	[SerializeField] private GameObject[] m_children;
 
-	// Use this for initialization
-	void Start ()
+	//Debug code to check if timer is longer than all child timers
+	void Start()
 	{
-		
+		foreach (GameObject child in m_children)
+		{
+			EnemyBulletPatternChild patternChild = child.GetComponent<EnemyBulletPatternChild>();
+
+			if (m_fTimer < patternChild.GetTimer())
+				Debug.LogWarning("Timer on " + gameObject.name + " is shorter than " + child.name + ", you should fix this immediately!");
+		}
+	}
+
+	// Use this for initialization
+	void Awake()
+	{
+		m_fCurrentTimer = 0.0f;
 	}
 	
 	// Update is called once per frame
-	void Update ()
+	void Update()
 	{
-		
+		m_fCurrentTimer += Time.deltaTime;
+
+		if (m_fCurrentTimer >= m_fTimer)
+		{
+			m_fCurrentTimer = 0.0f;
+
+			foreach(GameObject child in m_children)
+			{
+				EnemyBulletPatternChild patternChild = child.GetComponent<EnemyBulletPatternChild>();
+
+				patternChild.StartPattern();
+			}
+		}
 	}
 }
