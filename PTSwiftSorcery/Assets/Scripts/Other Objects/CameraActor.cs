0a6452990b81bf892ee31b2022eb5d8c28d6d23f
DenverLacey <36467790+DenverLacey@users.noreply.github.com>
2018-10-03 16:36:48 +1000

    Added the CameraActor and SplitActor scripts. - This commit was made by Denver

---
+++ PTSwiftSorcery/Assets/Scripts/Other Objects/CameraActor.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EZCameraShake;
+
+public class CameraActor : MonoBehaviour {
+
+	[Tooltip("Speed at which the camera will move to follow the player.")]
+	[SerializeField] private float m_fLerpSpeed;
+
+	[Tooltip("The magnitude of the movement of the camera as it follows the player.")]
+	[SerializeField] [Range(0f, 1f)] private float m_fLerpMagnitude;
+
+	[Tooltip("Magnitude of camera shake. The bigger the value, the larger the shake.")]
+	[SerializeField] private float m_fShakeMagnitude;
+
+	[Tooltip("Severity of camera shake.  The bigger the value, the more intense the shake.")]
+	[SerializeField] private float m_fShakeRoughness;
+
+	[Tooltip("Time taken to ramp up to full shake.")]
+	[SerializeField] private float m_fFadeInTime;
+
+	[Tooltip("Time taken to ramp down from full shake.")]
+	[SerializeField] private float m_fFadeOutTime;
+
+	private GameObject m_player;
+	private float m_fOriginX;
+
+	// Use this for initialization
+	void Start () {
+
+		m_player = GameObject.FindGameObjectWithTag("Player");
+
+		m_fOriginX = transform.parent.transform.localPosition.x;
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		float desiredX = (m_player.transform.localPosition.x + m_fOriginX) * m_fLerpMagnitude;
+
+		// move to desired position
+		transform.parent.transform.localPosition = Vector3.Lerp(transform.parent.transform.localPosition, new Vector3(desiredX, transform.parent.transform.localPosition.y, transform.parent.transform.localPosition.z), m_fLerpSpeed * Time.deltaTime);
+		
+	}
+
+	public void ShakeCamera() {
+
+		CameraShaker.Instance.ShakeOnce(m_fShakeMagnitude, m_fShakeRoughness, m_fFadeInTime, m_fFadeOutTime);
+
+	}
+}
---
+++ PTSwiftSorcery/Assets/Scripts/Other Objects/SplitActor.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplitActor : MonoBehaviour {
+
+	[Tooltip("Speed at which the split will move the player movement area.")]
+	[SerializeField] private float m_fMoveSpeed;
+
+	[Tooltip("Position of the centre of the left side path. (X axis)")]
+	[SerializeField] private float m_fLeftSideX;
+
+	[Tooltip("Position of the centre of the right side path. (X axis)")]
+	[SerializeField] private float m_fRightSideX;
+
+	[Tooltip("Left side level difficulty.")]
+	[SerializeField] private eLevelDifficulty m_leftDifficulty;
+
+	[Tooltip("Right side level difficulty.")]
+	[SerializeField] private eLevelDifficulty m_rightDifficulty;
+
+	private float m_fDesiredX;
+
+	private eLevelDifficulty m_desiredDifficulty;
+
+	void OnTriggerEnter(Collider other) {
+
+		Debug.Log(other.name + " has triggered " + name);
+
+		// check that other is the player
+		if (other.gameObject.tag == "Player") {
+			// calculate which side the player is on.
+			//if the player is on the right
+			if (other.gameObject.transform.position.x - transform.position.x > 0f) {
+				m_fDesiredX = m_fRightSideX;
+				m_desiredDifficulty = m_rightDifficulty;
+			}
+			else {
+				m_fDesiredX = m_fLeftSideX;
+				m_desiredDifficulty = m_leftDifficulty;
+			}
+
+			// set new difficulty
+			GameObject.FindGameObjectWithTag("LevelManager").GetComponent<LevelManager>().SetLevelDifficulty(m_desiredDifficulty);
+		}
+
+	}
+
+	void OnTriggerStay(Collider other) {
+
+		// check that other is the player
+		if (other.gameObject.tag == "Player") {
+			// get player movement area
+			GameObject moveArea = other.GetComponent<PlayerActor>().GetPlayerMovementArea();
+
+			// find direction to new centre
+			Vector3 v3DesiredDirection = new Vector3(m_fDesiredX, 0f, 0f) - moveArea.transform.position;
+			v3DesiredDirection = new Vector3(v3DesiredDirection.x, 0f, 0f);
+			v3DesiredDirection.Normalize();
+
+			// move in direction
+			moveArea.transform.Translate(v3DesiredDirection * m_fMoveSpeed * Time.deltaTime);
+
+			// if moveArea is more-or-less at destination, set its position to exactly that destination
+			if (Vector2.Distance(new Vector2(m_fDesiredX, 0f), new Vector2(moveArea.transform.position.x, 0f)) <= 0.1f) {
+				moveArea.transform.position = new Vector3(m_fDesiredX, moveArea.transform.position.y, moveArea.transform.position.z);
+			}
+
+		}
+
+	}
+}
