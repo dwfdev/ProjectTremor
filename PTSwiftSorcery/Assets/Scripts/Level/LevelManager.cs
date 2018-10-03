a6a5bd04bbaee7c9643b9bb400ad7c7112401797
DenverLacey <36467790+DenverLacey@users.noreply.github.com>
2018-10-03 16:51:02 +1000

    Added Packages folder for any Asset Store stuff and LevelManager script - This commit was made by Denver.

--- PTSwiftSorcery/Assets/Scripts/Level/LevelManager.cs
+++ PTSwiftSorcery/Assets/Scripts/Level/LevelManager.cs
@@ -3,19 +3,57 @@ using System.Collections.Generic;
 using UnityEngine;
 
 ///<summary>
-///Script Manager: Denver
-///Description:
-///
+///		Script Manager: Denver
+///		Description:	Handles the difficulty of the level, scroll direction and speed as well as moves the player
+///		Date Modified:	03/10/2018
 ///</summary>
-public class LevelManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+public enum eLevelDifficulty
+{
+	EASY,
+	NORMAL,
+	HARD
+}
+
+public class LevelManager : MonoBehaviour
+{
+
+	[Tooltip("The player movement area of the level.")]
+	[SerializeField]
+	private GameObject m_playerMovementArea;
+
+	[Tooltip("The speed at which the level will scroll.")]
+	public float m_fLevelScrollSpeed;
+
+	[Tooltip("The direction the level will scroll.")]
+	public Vector3 m_v3LevelScrollDirection;
+
+	private eLevelDifficulty m_levelDifficulty;
+
+	void Start()
+	{
+
+		// set tag to 'LevelManager'
+		gameObject.tag = "LevelManager";
+
+		// initialise level difficulty to EASY
+		m_levelDifficulty = eLevelDifficulty.EASY;
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void FixedUpdate()
+	{
+
+		// move the player's movement area
+		m_playerMovementArea.transform.Translate(m_v3LevelScrollDirection * m_fLevelScrollSpeed * Time.deltaTime);
+
+	}
+
+	public void SetLevelDifficulty(eLevelDifficulty difficulty)
+	{
+
+		// change level difficulty
+		m_levelDifficulty = difficulty;
+
 	}
 }
