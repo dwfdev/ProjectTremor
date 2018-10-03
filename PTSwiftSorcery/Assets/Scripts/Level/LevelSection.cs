10ed2cbd4482731a4e238e05398729451e6bb8ca
DenverLacey <36467790+DenverLacey@users.noreply.github.com>
2018-10-03 15:30:08 +1000

    Added LevelSection.cs - This commit was made by Denver

---
+++ PTSwiftSorcery/Assets/Scripts/Level/LevelSection.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSection : MonoBehaviour {
+
+	[Tooltip("List of all the enemies in this section.")]
+	public List<EnemyActor> m_enemiesList;
+
+	private bool m_bIsCleared;
+
+	void Start() {
+		
+		foreach(EnemyActor enemy in m_enemiesList) {
+			enemy.m_section = this;
+		}
+
+		m_bIsCleared = false;
+
+	}
+
+	void FixedUpdate() {
+		if (m_enemiesList.Count == 0) {
+			m_bIsCleared = true;
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+
+		// check that other is a player
+		if (other.tag == "Player") {
+			// set player's CurrentSection to this
+			other.gameObject.GetComponent<PlayerActor>().m_CurrentSection = this;
+
+			// activate all enemies of the section and set their target
+			foreach(EnemyActor enemy in m_enemiesList) {
+				enemy.Activate(other.gameObject, other.gameObject.GetComponent<PlayerActor>().GetPlayerMovementArea());
+			}
+		}
+
+	}
+
+	void OnTriggerExit(Collider other) {
+
+		// check that other is a player
+		if (other.tag == "PlayerMovementArea") {
+			// section is not cleared
+			if (!m_bIsCleared) {
+				// get the PlayerActor
+				GameObject playerMovementArea = other.gameObject;
+
+				// move player to the beginning of the section
+				playerMovementArea.transform.position = new Vector3(playerMovementArea.transform.position.x, playerMovementArea.transform.position.y, transform.position.z - (transform.localScale.z / 2f + playerMovementArea.transform.localScale.z / 2f));
+			}
+		}
+
+	}
+}
