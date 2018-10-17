223a16b80200ad22a527aa6ae53dabb71a01529e
DenverLacey <36467790+DenverLacey@users.noreply.github.com>
2018-10-17 16:03:27 +1100

    First draft of UISpells

--- PTSwiftSorcery/Assets/Scripts/UI/UISpells.cs
+++ PTSwiftSorcery/Assets/Scripts/UI/UISpells.cs
@@ -1,16 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+///<summary>
+///		Script Manager:	Denver
+///		Description:	Handles displaying which spell the player has selected
+///						graphically.
+///		Date Modified:	17/10/2018
+///</summary>
 
 public class UISpells : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	[Tooltip("Image of spells.")]
+	[SerializeField] private Image m_image;
+
+	[Tooltip("Border for selected spell.")]
+	[SerializeField] private Image m_selectedBorder;
+
+	private PlayerActor m_player;
+
+	void Start() {
+
+		// get instance of the player
+		m_player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerActor>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+	void FixedUpdate() {
 		
+		// change position of border based on selected spell type
+		switch(m_player.m_spellManager.m_eSpellType) {
+			case eSpellType.FIRE:
+				// set border position to top third
+				m_selectedBorder.rectTransform.position = new Vector3(m_selectedBorder.rectTransform.position.x, m_image.rectTransform.position.y + m_image.rectTransform.rect.height / 3, 0);
+				break;
+
+			case eSpellType.ICE:
+				// set border position to middle third
+				m_selectedBorder.rectTransform.position = m_image.rectTransform.position;
+				break;
+
+			case eSpellType.LIGHTNING:
+				// set border position to bottom third
+				m_selectedBorder.rectTransform.position = new Vector3(m_selectedBorder.rectTransform.position.x, m_image.rectTransform.position.y - m_image.rectTransform.rect.height / 3, 0);
+				break;
+
+			default:
+				Debug.LogError("Could not find player's selected spell type.", gameObject);
+				break;
+		}
 	}
 }
