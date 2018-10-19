f885723a4f65330f227cd706af83b88422aa71a9
DenverLacey <36467790+DenverLacey@users.noreply.github.com>
2018-10-19 15:20:26 +1100

    Actually added BossManager to the repository

---
+++ PTSwiftSorcery/Assets/Scripts/Enemy/BossManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossManager : MonoBehaviour {
+
+	[Tooltip("Enemy that is the final boss.")]
+	[SerializeField] private GameObject m_boss;
+
+	void FixedUpdate() {
+
+		if (!m_boss.activeSelf) {
+			SceneManager.Instance.SceneState = eSceneState.COMPLETE;
+		}
+	}
+}
