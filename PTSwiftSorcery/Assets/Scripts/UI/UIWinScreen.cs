74dd681813a885160d642d866cac0d1fad5aa0fc
DenverLacey <36467790+DenverLacey@users.noreply.github.com>
2018-10-19 15:15:25 +1100

    UIWinScreen script and meta data actually added to repository

---
+++ PTSwiftSorcery/Assets/Scripts/UI/UIWinScreen.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIWinScreen : MonoBehaviour {
+
+	void Start() {
+
+		if (GameObject.FindGameObjectWithTag("WinScreen")) {
+			GameObject winScreen = GameObject.FindGameObjectWithTag("WinScreen");
+			winScreen.SetActive(false);
+			SceneManager.Instance.WinScreen = winScreen;
+		}
+	}
+
+	public void OnNextLevelPressed() {
+
+		// move to next level
+		if (SceneManager.Instance.CurrentScene.index + 1 <= SceneManager.Instance.GameScenes.Length - 1) {
+			SceneManager.Instance.LoadScene(SceneManager.Instance.GameScenes[SceneManager.Instance.CurrentScene.index + 1], UnityEngine.SceneManagement.LoadSceneMode.Single);
+		}
+		else {
+			SceneManager.Instance.LoadScene(SceneManager.Instance.GetGameSceneWithName("MainMenu"), UnityEngine.SceneManagement.LoadSceneMode.Single);
+		}
+	}
+
+	public void OnQuitPressed() {
+
+		// move to main menu
+		SceneManager.Instance.LoadScene(SceneManager.Instance.GetGameSceneWithName("MainMenu"), UnityEngine.SceneManagement.LoadSceneMode.Single);
+	}
+}
