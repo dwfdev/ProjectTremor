bf5403a67a2fe7cdf6b41b859872da46945ac3ed
DenverLacey <36467790+DenverLacey@users.noreply.github.com>
2018-10-19 14:24:02 +1100

    DeathScreen Script added to repository

---
+++ PTSwiftSorcery/Assets/Scripts/UI/UIDeathScreen.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+///<summary>
+///		Script Manager:	Denver
+///		Description:	Handles functionality of the Death Screen
+///		Date Modified:	19/10/2018
+///</summary>
+
+public class UIDeathScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		if (GameObject.FindGameObjectWithTag("DeathScreen")) {
+			GameObject deathScreen = GameObject.FindGameObjectWithTag("DeathScreen");
+			deathScreen.SetActive(false);
+			SceneManager.Instance.DeathScreen = deathScreen;
+		}
+	}
+	
+	public void OnRestartPressed() {
+
+		// load current scene again
+		SceneManager.Instance.ReloadCurrentScene();
+	}
+
+	public void OnQuitPressed() {
+
+		// load Main Menu scene
+		SceneManager.Instance.LoadScene(SceneManager.Instance.GetGameSceneWithName("MainMenu"), UnityEngine.SceneManagement.LoadSceneMode.Single);
+	}
+}
