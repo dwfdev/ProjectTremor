7ff5b1001f1b0a0d05de08a89c01ca7258f74af1
DenverLacey <36467790+DenverLacey@users.noreply.github.com>
2018-10-25 10:05:29 +1100

    UIHealthBar script is now ready for testing

--- PTSwiftSorcery/Assets/Scripts/UI/UIHealthBar.cs
+++ PTSwiftSorcery/Assets/Scripts/UI/UIHealthBar.cs
@@ -3,6 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+///<summary>
+///		Script Manager:	Denver
+///		Description:	Handles instantiating a health bar for enemy,
+///						moving that health bar with enemy and moving
+///						it up in the hierarchy
+///		Date Modified:	25/10/2018
+///</summary>
+
 public class UIHealthBar : MonoBehaviour {
 
 	[Tooltip("Health Bar prefab.")]
@@ -10,6 +18,8 @@ public class UIHealthBar : MonoBehaviour {
 
 	[Tooltip("Y axis offset of health bar.")]
 	[SerializeField] private float m_fOffset;
+
+	private EnemyActor m_enemyActor;
 
 	private Image m_healthBar;
 	private Image m_healthBarFilled;
@@ -20,15 +30,21 @@ public class UIHealthBar : MonoBehaviour {
 		// instantiate health bar
 		m_healthBar = Instantiate(m_healthBarPrefab, FindObjectOfType<Canvas>().transform).GetComponent<Image>();
 		m_healthBarFilled = new List<Image>(m_healthBar.GetComponentsInChildren<Image>()).Find(img => img != m_healthBar);
+
+		// get EnemyActor
+		m_enemyActor = GetComponent<EnemyActor>();
+
+		// make sure health bar is higher than other ui elements in hierarchy
+		m_healthBar.transform.SetSiblingIndex(0);
 	}
 	
 	void FixedUpdate () {
 		
 		// move the health bar to be slightly above gameObject on canvas
-		m_healthBar.transform.position = Camera.main.WorldToScreenPoint(transform.position + Vector3.up * m_fOffset);
+		m_healthBar.transform.position = Camera.main.WorldToScreenPoint(transform.position + Vector3.forward * m_fOffset);
 
 		// fill bar according to enemies health
-		m_healthBarFilled.fillAmount = (float)GetComponent<EnemyActor>().m_nCurrentHealth / (float)GetComponent<EnemyActor>().m_nHealth;
+		m_healthBarFilled.fillAmount = (float)m_enemyActor.m_nCurrentHealth / (float)m_enemyActor.m_nHealth;
 	}
 
 	public void DestroyHealthBar() {
