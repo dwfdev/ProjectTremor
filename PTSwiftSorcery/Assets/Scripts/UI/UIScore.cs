cef381257ad3ade30be1b7d2bc489496654984f6
DenverLacey <36467790+DenverLacey@users.noreply.github.com>
2018-10-25 11:00:30 +1100

    UIScore is now ready for testing

--- PTSwiftSorcery/Assets/Scripts/UI/UIScore.cs
+++ PTSwiftSorcery/Assets/Scripts/UI/UIScore.cs
@@ -1,16 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+///<summary>
+///		Script Manager:	Denver
+///		Description:	Displays the player's score on the canvas
+///		Date Modified:	25/10/2018
+///</summary>
 
 public class UIScore : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	[Tooltip("Text box that shows score.")]
+	[SerializeField] private Text m_scoreTextBox;
+
+	[Tooltip("Text box that shows multiplier.")]
+	[SerializeField] private Text m_multiplierTextBox;
+
+	void FixedUpdate() {
+
+		// change score text box text
+		m_scoreTextBox.text = ScoreManager.Instance.m_lScore.ToString();
+
+		// change multiplier text box text
+		m_multiplierTextBox.text = "x" + ScoreManager.Instance.m_fMultiplier.ToString();
 	}
 }
