f16a4fe4a235d5ae1868b985230f6da5ea60b1fe
DenverLacey <36467790+DenverLacey@users.noreply.github.com>
2018-10-26 11:46:45 +1100

    Bomb is now an AOE attack. Ready for testing

--- PTSwiftSorcery/Assets/Scripts/Other Objects/BombActor.cs
+++ PTSwiftSorcery/Assets/Scripts/Other Objects/BombActor.cs
@@ -1,11 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 ///<summary>
 ///		Script Manager: Denver
 ///		Description:	Handles the functionality of the bomb.
-///		Date Modified:	25/10/2018
+///		Date Modified:	26/10/2018
 ///</summary>
 
 public class BombActor : MonoBehaviour {
@@ -16,34 +17,127 @@ public class BombActor : MonoBehaviour {
 	[Tooltip("How much score will be awarded for each bullet destroyed.")]
 	[SerializeField] private int m_nBulletScore;
 
+	[Tooltip("Maximum bomb range.")]
+	[SerializeField] private float m_fMaximumRange;
+
+	[Tooltip("Time bomb lasts.")]
+	[SerializeField] private float m_fDuration;
+
+	[Tooltip("Starting distance.")]
+	[SerializeField] [Range(1f, float.MaxValue)] private float m_fStartRange;
+
+	[HideInInspector]
+	public bool m_bIsExploding;
+
+	private float m_fCurrentTime;
+
+	private float m_fChangeInValue;
+
+	private float m_fCurrentRange;
+
+	private float m_fOldRange;
+
 	private PlayerActor m_player;
 
 	private GameObject[] m_bullets;
 
 	private CameraActor m_camera;
 
-	public void Boom() {
+	private MeshRenderer m_renderer;
 
-		// get player
-		m_player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerActor>();
+	private SphereCollider m_collider;
 
-		// shake camera
-		m_camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraActor>();
-		m_camera.ShakeCamera();
+	void Start() {
 
-		// get all bullets
-		m_bullets = GameObject.FindGameObjectsWithTag("EnemyBullet");
+		// get components
+		m_renderer = GetComponent<MeshRenderer>();
+		m_collider = GetComponent<SphereCollider>();
 
-		foreach (GameObject bullet in m_bullets) {
-			ScoreManager.Instance.AddScore(m_nBulletScore);
-			Destroy(bullet);
+		// initialise components
+		m_renderer.enabled = false;
+		m_collider.enabled = false;
+
+		// initialise easing function variables
+		m_fCurrentTime = 0f;
+		m_fChangeInValue = 0f;
+		m_fCurrentRange = m_fStartRange;
+		m_fOldRange = 0f;
+	}
+
+	void FixedUpdate() {
+
+		// if bomb should blow up
+		if (m_bIsExploding) {
+			// enable renderer and collider
+			m_renderer.enabled = true;
+			m_collider.enabled = true;
+
+			// calculate change in value
+			m_fChangeInValue = m_fCurrentRange - m_fOldRange;
+
+			m_fCurrentTime += Time.deltaTime;
+
+			// calcualte current range using easing function
+			m_fCurrentRange = EaseOutExpo(m_fCurrentTime, m_fStartRange, m_fChangeInValue, m_fDuration);
+
+			// Clamp range
+			m_fCurrentRange = Mathf.Clamp(m_fCurrentRange, m_fStartRange, m_fMaximumRange);
+
+			// display range visually using bomb effect
+			transform.localScale = new Vector3(m_fCurrentRange, 0.1f, m_fCurrentRange);
+			Debug.Log(m_fCurrentRange);
 		}
+	}
 
-		// get all active enemies in current section
-		foreach (EnemyActor enemy in m_player.m_currentSection.m_enemiesList) {
-			if (enemy.m_bIsActive && enemy.m_bIsAlive) {
-				enemy.TakeDamage(m_nBombDamage);
-			}
+	float EaseOutExpo(float t, float b, float c, float d) {
+
+		// calculate new range using easing function
+		return c * (-Mathf.Pow(2, -10 * t / d) + 1) + b;
+	}
+
+	public void StartBomb() {
+
+		// invoke stop bomb
+		Invoke("StopBomb", m_fDuration);
+
+		m_bIsExploding = true;
+	}
+
+	void StopBomb() {
+
+		// set blow up to false
+		m_bIsExploding = false;
+
+		// disable components
+		m_renderer.enabled = false;
+		m_collider.enabled = false;
+
+		// reset easing function variables
+		m_fCurrentTime = 0f;
+		m_fChangeInValue = 0f;
+		m_fCurrentRange = m_fStartRange;
+		m_fOldRange = 0f;
+
+		// reset bomb effect
+		transform.localScale = new Vector3(1, 0.1f, 1);
+
+		Debug.Log("Bomb has stopped");
+	}
+
+	void OnTriggerEnter(Collider other) {
+
+		// check that other is an EnemyBullet
+		if (other.tag == "EnemyBullet") {
+			ScoreManager.Instance.AddScore(m_nBulletScore);
+			Destroy(other.gameObject);
+		}
+	}
+
+	void OnTriggerStay(Collider other) {
+
+		// check that other is an Enemy
+		if (other.tag == "Enemy") {
+			other.gameObject.GetComponent<EnemyActor>().TakeDamage(m_nBombDamage);
 		}
 	}
 }
