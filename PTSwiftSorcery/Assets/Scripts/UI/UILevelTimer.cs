3f6663bee59d25221389ea1fecc9efd70dafcb5c
DenverLacey <36467790+DenverLacey@users.noreply.github.com>
2018-11-02 13:56:26 +1100

    Level Timer UI script ready for testing

---
+++ PTSwiftSorcery/Assets/Scripts/UI/UILevelTimer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UILevelTimer : MonoBehaviour {
+
+	[Tooltip("Text object that will display the timer.")]
+	[SerializeField] private Text m_timerText;
+
+	void FixedUpdate() {
+
+		string text = ((int)(Time.timeSinceLevelLoad / 60f)).ToString("00") + ':' + 
+		((int)(Time.timeSinceLevelLoad % 60f)).ToString("00") + "." + 
+		(((Time.timeSinceLevelLoad % 60f) - ((int)(Time.timeSinceLevelLoad % 60f))) * 100).ToString("00");
+		m_timerText.text = text;
+	}
+}
