f9cf561a3c743c7076a834efa317d14832fb3894
ThePyromage <the.pyromagi@gmail.com>
2018-11-14 14:36:39 +1100

    Skimming implemented (also someone forgot to add a meta file again)

---
+++ PTSwiftSorcery/Assets/Scripts/Player/PlayerSkimming.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSkimming : MonoBehaviour
+{
+	[Tooltip("How many points should be gained per second while skimming")]
+	[SerializeField] private long m_lSkimPoints;
+
+	private bool m_bIsSkimming;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(m_bIsSkimming)
+		{
+			long skimPoints = (long)(m_lSkimPoints * Time.deltaTime);
+			Debug.Log(skimPoints);
+			ScoreManager.Instance.AddScore(skimPoints);
+		}
+	}
+
+	private void OnTriggerStay(Collider other)
+	{
+		if (other.tag == "EnemyBullet")
+		{
+			m_bIsSkimming = true;
+			//Debug.Log("Skimming!");
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if(other.tag == "EnemyBullet")
+		{
+			m_bIsSkimming = false;
+			//Debug.Log("Stopped skimming!");
+		}
+	}
+}
