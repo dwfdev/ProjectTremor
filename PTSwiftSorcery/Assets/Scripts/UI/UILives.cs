c36a52d867fd1887fdf9fdb426664fa430f459a7
DenverLacey <36467790+DenverLacey@users.noreply.github.com>
2018-11-22 09:39:01 +1100

    hitbox indicator of witch and wizard prefabs now visible, UILives adapted to work with new difficulty functionality

--- PTSwiftSorcery/Assets/Scripts/UI/UILives.cs
+++ PTSwiftSorcery/Assets/Scripts/UI/UILives.cs
@@ -18,20 +18,40 @@ public class UILives : MonoBehaviour {
 	[SerializeField] private Sprite m_lostLifeSprite;
 
 	[Tooltip("List of Images.")]
-	[SerializeField] private Image[] m_imageHolders;
+	[SerializeField] private List<Image> m_imageHolders;
 
 	private PlayerActor m_player;
 
 	void Start() {
 
-		m_player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerActor>();
+		// get reference to the player
+		m_player = FindObjectOfType<PlayerActor>();
 
+		// check image holders set up properly
+		if (m_imageHolders.Count != SceneManager.Instance.Difficulties[0].numOfLives) {
+			Debug.LogError("Incorrect number of image holders.", gameObject);
+		}
+		else {
+			for (int i = m_imageHolders.Count; i != 0; --i) {
+				// if heart is redudent
+				if (i > SceneManager.Instance.CurrentDifficulty.numOfLives) {
+					// get referene to heart
+					Image image = m_imageHolders[i - 1];
+
+					// remove it from list
+					m_imageHolders.Remove(image);
+
+					// destroy heart
+					Destroy(image);
+				}
+			}
+		}
 	}
 
 	void OnGUI() {
 
 		// change each image holders sprites such that it matches player's lives
-		for (int i = 0; i < m_imageHolders.Length; ++i) {
+		for (int i = 0; i < m_imageHolders.Count; ++i) {
 			if (i <= m_player.m_nLives - 1) {
 				m_imageHolders[i].sprite = m_lifeSprite;
 			}
