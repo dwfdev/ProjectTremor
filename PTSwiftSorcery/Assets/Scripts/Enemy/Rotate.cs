0f50e2eb5c37a5d7b287d25b638cc65a1fbff6b1
ThePyromage <the.pyromagi@gmail.com>
2018-11-23 15:52:21 +1100

    Rotation script added

---
+++ PTSwiftSorcery/Assets/Scripts/Enemy/Rotate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+	[Tooltip("How fast this rotates")]
+	[SerializeField] private float m_fRotationSpeed;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.Rotate(Vector3.up, m_fRotationSpeed);
+	}
+}
