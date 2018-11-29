380b9a63b62f0756a72526361e52aae336fbd846
DenverLacey <36467790+DenverLacey@users.noreply.github.com>
2018-11-29 17:04:21 +1100

    stuff was DONE

--- PTSwiftSorcery/Assets/Scripts/UI/UIHitEffect.cs
+++ PTSwiftSorcery/Assets/Scripts/UI/UIHitEffect.cs
@@ -5,39 +5,40 @@ using UnityEngine.UI;
 
 public class UIHitEffect : MonoBehaviour {
 
-	[Tooltip("Hit Effect Texture.")]
-	[SerializeField] private Texture m_hitEffectTexture; 
+	[Tooltip("default Material")]
+	[SerializeField] private Material m_defaultMaterial;
+
+	[Tooltip("Hit Material.")]
+	[SerializeField] private Material m_hitMaterial; 
 
 	[Tooltip("How long the texture is drawn for.")]
 	[SerializeField] private float m_fEffectDuration;
 
-	private bool m_bIsShowing;
+	private Renderer[] m_renderers;
+
+	void Start() {
+		
+		m_renderers = GetComponentsInChildren<Renderer>();
+	}
 
 	public void Show() {
 
-		m_bIsShowing = true;
-
 		if (!IsInvoking("Hide")) {
+			// change material
+			foreach (Renderer rend in m_renderers) {
+				rend.material = m_hitMaterial;
+			}
+
 			Invoke("Hide", m_fEffectDuration);
 		}
 	}
 
 	void Hide() {
 
-		m_bIsShowing = false;
-	}
-
-	void OnGUI() {
-
-		if (m_bIsShowing) {
-			// calculate desired position for texture
-			Vector2 desiredPosition = Camera.main.WorldToScreenPoint(transform.position);
-
-			// calculate desired Texture Rectangle
-			Rect desiredRect = new Rect(desiredPosition.x - (m_hitEffectTexture.width / 2), Screen.height - desiredPosition.y - (m_hitEffectTexture.height / 2), m_hitEffectTexture.width, m_hitEffectTexture.height);
-
-			// draw texture
-			GUI.DrawTexture(desiredRect, m_hitEffectTexture);
+		// change it back
+		foreach (Renderer rend in m_renderers) {
+			rend.material = m_defaultMaterial;
 		}
 	}
 }
+
