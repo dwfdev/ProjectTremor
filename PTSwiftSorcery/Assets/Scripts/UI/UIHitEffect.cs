2c9cebd61adaaebfd2c4a18fb8c000ecbce9d70c
DenverLacey <36467790+DenverLacey@users.noreply.github.com>
2018-11-30 09:28:53 +1100

    hit effect now automatically handles resetting materials

--- PTSwiftSorcery/Assets/Scripts/UI/UIHitEffect.cs
+++ PTSwiftSorcery/Assets/Scripts/UI/UIHitEffect.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UIHitEffect : MonoBehaviour {
+/// <summary>
+///		Script Manager:	Denver
+///		Description:	Handles hit marker effect
+/// </summary>
 
-	[Tooltip("default Material")]
-	[SerializeField] private Material m_defaultMaterial;
+public struct sMaterial
+{
+	public Renderer renderer;
+	public Material[] defaultMaterials;
+}
+
+public class UIHitEffect : MonoBehaviour {
 
 	[Tooltip("Hit Material.")]
 	[SerializeField] private Material m_hitMaterial; 
@@ -14,19 +22,29 @@ public class UIHitEffect : MonoBehaviour {
 	[Tooltip("How long the texture is drawn for.")]
 	[SerializeField] private float m_fEffectDuration;
 
-	private Renderer[] m_renderers;
+	private List<sMaterial> m_sMaterials = new List<sMaterial>();
 
 	void Start() {
-		
-		m_renderers = GetComponentsInChildren<Renderer>();
+
+		// get all renderers in object
+		Renderer[] renderers = GetComponentsInChildren<Renderer>();
+
+		foreach (Renderer rend in renderers) {
+			sMaterial newSmat = new sMaterial() {
+				renderer = rend,
+				defaultMaterials = rend.materials
+			};
+
+			m_sMaterials.Add(newSmat);
+		}
 	}
 
 	public void Show() {
 
 		if (!IsInvoking("Hide")) {
 			// change material
-			foreach (Renderer rend in m_renderers) {
-				rend.material = m_hitMaterial;
+			foreach (sMaterial smat in m_sMaterials) {
+				smat.renderer.material = m_hitMaterial;
 			}
 
 			Invoke("Hide", m_fEffectDuration);
@@ -36,8 +54,8 @@ public class UIHitEffect : MonoBehaviour {
 	void Hide() {
 
 		// change it back
-		foreach (Renderer rend in m_renderers) {
-			rend.material = m_defaultMaterial;
+		foreach (sMaterial smat in m_sMaterials) {
+			smat.renderer.materials = smat.defaultMaterials;
 		}
 	}
 }
